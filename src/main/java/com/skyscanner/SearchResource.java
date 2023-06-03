9b28ecc51b3594b9add3517862622b57c9b585b8
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2023-06-03 12:20:38 +0530

    Add files via upload

---
+++ src/main/java/com/skyscanner/SearchResource.java
@@ -0,0 +1,10 @@
+package com.skyscanner;
+
+import javax.naming.directory.SearchResult;
+import java.util.List;
+
+public class SearchResource {
+
+    public SearchResource(List<SearchResult> searchResults) {
+    }
+}
